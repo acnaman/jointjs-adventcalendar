2a104d8eb6404327687369c7c28e07181355e988
acnaman <acnadin01@gmail.com>
2023-12-25 20:41:03 +0900

    作りかけコンポーネント追加（ビルドエラー回避）

---
+++ src/components/EditCalendarDialog.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="modal">
+    <p>タイトル</p>
+    <input v-bind="article.title" />
+
+    <p>URL</p>
+    <input v-bind="article.url" />
+
+    <div>
+      <button @click="cancel">キャンセル</button>
+      <button @click="update">更新する</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {title: String, url: String},
+  },
+  data() {
+    return {
+      graph: null,
+      paper: null,
+      startPos: 20,
+    }
+  },
+
+  methods: {
+    cancel() {
+      this.$emit('cancel');
+    },
+    update() {
+      this.$emit('update');
+    }
+  },
+}
+</script>
+
+<style scoped>
+.modal {
+  z-index: 100;
+  width: 300px;
+  height: 300px;
+  margin: 0;
+}
+</style>
